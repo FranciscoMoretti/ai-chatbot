34576e8fdab9cd4ad6c30b4a79c1e92d541df3c4
Francisco Moretti <franciscoemoretti@gmail.com>
2024-11-23 10:29:06 +0000

    added anthropic

--- lib/ai/index.ts
+++ lib/ai/index.ts
@@ -1,11 +1,16 @@
 import { openai } from '@ai-sdk/openai';
+import { anthropic } from '@ai-sdk/anthropic';
 import { experimental_wrapLanguageModel as wrapLanguageModel } from 'ai';
 
 import { customMiddleware } from './custom-middleware';
+import { models } from './models';
 
 export const customModel = (apiIdentifier: string) => {
+  const provider = models.find((model) => model.apiIdentifier === apiIdentifier)
+    ?.provider;
+
   return wrapLanguageModel({
-    model: openai(apiIdentifier),
+    model:provider === 'openai' ? openai(apiIdentifier) : anthropic(apiIdentifier),
     middleware: customMiddleware,
   });
 };
--- lib/ai/models.ts
+++ lib/ai/models.ts
@@ -2,6 +2,7 @@
 
 export interface Model {
   id: string;
+  provider: 'openai' | 'anthropic';
   label: string;
   apiIdentifier: string;
   description: string;
@@ -13,12 +14,21 @@ export const models: Array<Model> = [
     label: 'GPT 4o mini',
     apiIdentifier: 'gpt-4o-mini',
     description: 'Small model for fast, lightweight tasks',
+    provider: 'openai',
   },
   {
     id: 'gpt-4o',
     label: 'GPT 4o',
     apiIdentifier: 'gpt-4o',
     description: 'For complex, multi-step tasks',
+    provider: 'openai',
+  },
+  {
+    id: 'claude-3-5-sonnet',
+    label: 'Claude 3.5 Sonnet',
+    apiIdentifier: 'claude-3-5-sonnet-20241022',
+    description: 'For complex, multi-step tasks, with documents',
+    provider: 'anthropic',
   },
 ] as const;
 
